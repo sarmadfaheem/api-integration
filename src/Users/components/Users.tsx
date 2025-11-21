14d58496409a593993de2125aa4dbada79b44a72
Sarmad Faheem <muhammadsarmad302@gmail.com>
2025-11-21 16:25:27 +0500

    Make User Component

--- src/Users/components/Users.tsx
+++ src/Users/components/Users.tsx
@@ -1,45 +1,51 @@
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { Autocomplete, Stack, TextField } from "@mui/material";
+import { schema, type Schema } from "./services/types/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 function Users() {
-  const UsersForm = useForm<{ email: string }>({
+  const UsersForm = useForm<Schema>({
     mode: "all",
+    resolver: zodResolver(schema),
   });
 
   const {
     register,
     formState: { errors },
-    handleSubmit,
   } = UsersForm;
 
-  const onSubmit = () => {
-    console.log("Submitted");
-  };
-
   return (
-    <div className="main-container">
+    <div className="main-container bg-white">
       <div className="my-10 text-center space-x-15">
-        <Link to="/" className="font-bold">
+        <Link to="/" className="text-black font-bold">
           Home
         </Link>
       </div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label className="text-white" htmlFor="email">
-          Email
-        </label>
-        <br />
-        <input
-          className="bg-white text-black px-2.5"
-          type="text"
-          placeholder="Email"
-          {...register("email", {
-            required: { value: true, message: "The email is Required" },
-            maxLength: { value: 10, message: "Too many characters" },
-          })}
+      <div>
+        <Stack sx={{ gap: 2 }}>
+          <TextField
+            {...register("name")}
+            label="Name"
+            error={!!errors.name}
+            helperText={errors.name?.message}
+          />
+          <TextField
+            {...register("email")}
+            label="Email"
+            error={!!errors.email}
+            helperText={errors.email?.message}
+          />
+        </Stack>
+        <Autocomplete
+          options={[
+            { id: 1, label: "Texas" },
+            { id: 2, label: "California" },
+            { id: 3, label: "Los Angeles" },
+          ]}
+          renderInput={(params) => <TextField {...params} label="States" />}
         />
-
-        <p className="text-white">{errors.email?.message}</p>
-      </form>
+      </div>
     </div>
   );
 }
--- src/Users/components/services/types/schema.ts
+++ src/Users/components/services/types/schema.ts
@@ -10,3 +10,5 @@ export const schema = z.object({
       message: "Email not Valid",
     }),
 });
+
+export type Schema = z.infer<typeof schema>;
